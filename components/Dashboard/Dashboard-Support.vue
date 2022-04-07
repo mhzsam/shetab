83eba5d50c2b0c1637720d40b0cac7dfc455c3e3
mohamad zarabi <zarab@gmail.com>
2022-04-07 15:21:04 +0430

    Update Dashboard-Support.vue

--- components/Dashboard/Dashboard-Support.vue
+++ components/Dashboard/Dashboard-Support.vue
@@ -2,82 +2,140 @@
   <div>
     <v-row no-gutters>
       <v-col cols="12" md="4">
-        <v-card class="ma-5" height="350px">
-          
-           
-          <div >
+        <v-card class="ma-5" height="370px">
+          <div>
             <v-card-title primary-title>
-              <v-icon class="mx-4">mdi-account-circle</v-icon>
-              مشخصات کاربری
+              <v-icon class="mx-4">mdi-account-supervisor-outline</v-icon>
+              اخرین تیکن ها
             </v-card-title>
             <v-divider inset></v-divider>
-
-            <v-card-text class="black--text mr-14" height="220px">
-              <p>نام : {{ firstName }}</p>
-              <p>نام خانوادگی : {{ lastName }}</p>
-              <p>تاریخ عضویت : {{ registerDate }}</p>
-              <p>جنسیت : {{ gender }}</p>
-              <p>ایمیل : {{ email }}</p>
-            </v-card-text>
-            <v-flex d-flex justify-center>
+            <v-flex mt-5 d-flex justify-center>
               <v-btn
                 width="70%"
                 class=""
+                height="50"
+                elevation="3"
+                color="#f4f6fc"
+                right
+                @click="dialogOn"
+                >تیکت 1 تاریخ 12/12/01</v-btn
+              >
+            </v-flex>
+            <v-flex mt-5 d-flex justify-center>
+              <v-btn
+                width="70%"
+                class=""
+                height="50"
+                elevation="3"
+                color="#f4f6fc"
+                @click="dialogOn"
+                >تیکت 2 تاریخ 12/12/01</v-btn
+              >
+            </v-flex>
+            <v-flex mt-5 d-flex justify-center>
+              <v-btn
+                width="70%"
+                class=""
+                height="50"
+                elevation="3"
+                color="#f4f6fc"
+                @click="dialogOn"
+                >تیکت 3 تاریخ 12/12/01</v-btn
+              >
+            </v-flex>
+            <v-flex mt-5 d-flex justify-center>
+              <v-btn
                 dark
-                elevation="2"
-                color="orange"
-                @click="editeUserProfile"
-                >ویرایش</v-btn
+                width="70%"
+                class=""
+                height="50"
+                elevation="3"
+                color="#5b7bb6"
+                @click="dialogOn"
+                >مشاهده همه</v-btn
               >
             </v-flex>
           </div>
         </v-card>
       </v-col>
       <v-col cols="12" md="8">
-        <v-card class="ma-5" height="350px">
+        <v-card class="ma-5" height="370px">
           <v-card-title primary-title>
-            <v-icon class="mx-4">mdi-face-man-profile</v-icon>
-           ارسال تیکت
+            <v-icon class="mx-4">mdi-face-agent</v-icon>
+            ارسال تیکت
           </v-card-title>
           <v-divider inset></v-divider>
-          <div v-if="esiteUserAvatar">
-            <v-flex class="mt-5 pt-10" d-flex justify-center>
-              <v-file-input
-                class="ma-4"
-                :rules="avatarRules"
-                accept="image/png, image/jpeg, image/bmp"
-                placeholder="بارگیری عکس"
-                prepend-icon="mdi-paperclip"
-                label="انتخاب عکس"
+          <div class="mr-12 px-6">
+            <v-flex class="mt-5">
+              <v-text-field
+                background-color="white"
+                filled
+                prefix="موضوع:"
+                color="#ccdcf2"
                 outlined
-                rounded
-                v-model="files"
-                
-              ></v-file-input>
-            </v-flex>
-            <v-flex class="" d-flex justify-center>
-              <v-btn width="70%" class="" dark elevation="2" color="green"
-              @click="uploadAvatar"
-                >ذخیره</v-btn
-              >
-            </v-flex>
-          </div>
-          <div v-if="!esiteUserAvatar">
-            <v-flex class="mt-5" d-flex justify-center>
-              <v-avatar max-width="180" size="180">
-                <v-img src="img/avatar/avatar.png"></v-img>
-              </v-avatar>
-            </v-flex>
-            <v-flex class="mt-5" d-flex justify-center>
-              <v-btn width="70%" class="" dark elevation="2" color="orange"
-              @click="changeAvatar"
-                >ویرایش</v-btn
-              >
+                hint="موضوع تیکت را وارد کنید "
+                single-line
+              ></v-text-field>
+              <v-textarea
+              background-color="white"
+              outlined
+               filled
+                clearable
+                clear-icon="mdi-close-circle"
+                label="متن"
+                placeholder="متن تیکت خود را تایپ کنید "
+              ></v-textarea>
+              
             </v-flex>
           </div>
+
+          <v-flex class="mt-5" d-flex justify-center>
+            <v-btn width="70%" class="" dark elevation="2" color="orange"
+              >ویرایش</v-btn
+            >
+          </v-flex>
         </v-card>
       </v-col>
-      
+    </v-row>
+
+    <!-- dialog for tiket  -->
+
+    <v-row justify="center">
+      <v-dialog v-model="dialog" width="70%">
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">تیکت شماره 123</span>
+          </v-card-title>
+
+          <v-card-text>
+            موضوع : تیکت 1
+            <br />
+            تاریخ : 12/12/1401
+            <br />
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
+            و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای
+            زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و
+            متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان
+            رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد
+            کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه
+            راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل
+            حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود
+            طراحی اساسا مورد استفاده قرار گیرد.
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="red darken-1" text @click="dialog = false">
+              بستن
+            </v-btn>
+            <v-btn color="green darken-1" text @click="dialog = false">
+              پاسخ
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-row>
   </div>
 </template>
@@ -87,8 +145,7 @@ export default {
   name: "Dashboard-Support",
   data() {
     return {
-     
-   
+      dialog: false,
       avatarRules: [
         (value) => !value || value.size < 200000 || "کمتر از 2 مگابایت",
       ],
@@ -99,8 +156,9 @@ export default {
     };
   },
   methods: {
-    
-    
+    dialogOn() {
+      this.dialog = !this.dialog;
+    },
   },
 };
 </script>

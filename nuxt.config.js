cef6133aa0c4098fd1131144dc663bd9bb0967d2
mohammad <mhzsam@gmail.com>
2022-05-07 17:11:53 +0430

    again

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  Global page headers: https://go.nuxtjs.dev/config-head
+  // Global page headers: https://go.nuxtjs.dev/config-head
   server: {
     port: 8000, // default: 3000
     host: '94.182.186.227', // default: localhost,
